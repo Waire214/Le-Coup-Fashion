7442fcc8eff2808417d46a2ac7ebf6a80c9fe159
Waire <waire.tega@gmail.com>
2020-07-06 14:04:37 +0100

    init

---
+++ index.js
@@ -0,0 +1,9 @@
+var bespoke = "We are doing it";
+console.log(bespoke);
+function atiku(buba, shokoto){
+
+    var result = buba + " and " + shokoto;
+    console.log(result);
+}
+atiku("One inch","twenty five inches");
+console.log(atiku.length);
