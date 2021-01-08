24280da18d2af5ee3e0feea85fa9da72e2050f44
Waire <waire.tega@gmail.com>
2021-01-08 09:33:23 +0100

    name change

--- index.js
+++ index.js
@@ -1,9 +1,33 @@
-var bespoke = "We are doing it";
-console.log(bespoke);
-function atiku(buba, shokoto){
-
-    var result = buba + " and " + shokoto;
-    console.log(result);
-}
-atiku("One inch","twenty five inches");
-console.log(atiku.length);
+const a = 1;
+const b = new String("Hello World");
+const c = "Alexa";
+const d = c.length;
+const e = c.toUpperCase();
+const f = c.charAt(3);
+const g = c.indexOf("A");
+const h = c.lastIndexOf("e");
+const i = c.includes("a");
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(d);
+console.log(e);
+console.log(f);
+console.log(g);
+console.log(h);
+console.log(i);
+const concatenation = "Hello".concat(" ", "World", "!");
+console.log(concatenation);
+const repeat = "Hello".repeat(2);
+console.log(repeat);
+const templateLiterals = `Tosin ${concatenation}`;
+console.log(templateLiterals);
+const number1 = 42;
+console.log(typeof number1);
+console.log(Number.isInteger(42));
+document.getElementById("demo").innerHTML = alert("I am learning JavaScript and I'm excited to have you here");
+let person = {
+    firstName: Oluwatosin,
+    middleName: Oghenewaire,
+    surName: Thompson
+};
