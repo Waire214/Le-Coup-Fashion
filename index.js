293a2a187a84bcd352fca446d2e81771acd6ff2c
Waire <waire.tega@gmail.com>
2021-01-27 09:59:25 +0100

    name change

--- index.js
+++ index.js
@@ -101,9 +101,172 @@ let cars = [
 cars.sort(function (a,b) {
     console.log(a.year - b.year);
 });
-let num1 = [4,5,6,7,8];
+let textWrite = "";
+let num1 = ["is 4","is 5","is 6","is 7"];
 num1.forEach(myEachFunc);
-function myEachFunc(value){
-    console.log("This is number " + value);
+function myEachFunc(value, num1){
+    textWrite = "<p>This number " + value + "</p>";
+    console.log(num1);
+    console.log(textWrite);
 }
 myEachFunc();
+//get today's date
+function today(format){
+    format = new Date();
+    const days = format.getDate();
+    const months = format.getMonth() + 1;
+    const years = format.getFullYear();
+    return `${days}/${months}/${years}`;
+}
+console.log(today());
+function newExercise(x,y) {
+    if(x === 100 || x + y === 100){
+        return true;
+    }else{
+        return false;
+    }
+}
+console.log(newExercise(49,51));
+
+//get the extension of a filename.
+const getFileExtension = (str) => str.slice(str.lastIndexOf("."));
+console.log(getFileExtension("index.html"));
+function addNew(str) {
+    if(str.indexOf("New!") !== 0){
+        let x = `New! ${str}`
+        return x;
+    }else{
+        return str;
+    }
+}
+console.log(addNew("It is new."));
+console.log(addNew("New!, yay"));
+
+function removeAddStrings(str) {
+    if (str.length < 3){
+        return str;
+    }else{
+    return str.slice(0,3) + str.slice(-3);
+    }
+}
+console.log(removeAddStrings("The living of"));
+console.log(removeAddStrings("The"));
+console.log(removeAddStrings("of"));
+
+function halfString(str){
+    if (str.length % 2 === 0){
+        let x = str.length / 2;
+       return str.slice(0,x);
+    }else{
+        return "Not even";
+    }
+}
+console.log(halfString("1234567"));
+console.log(halfString("123456"));
+function removeFirstCharacter(str1,str2){
+    return `${str1.slice(1)} ${str2.slice(1)}`;
+}
+console.log(removeFirstCharacter("The", "She"));
+console.log(removeFirstCharacter("SJS", "SNINJA"));
+
+function closeTo100(x, y){
+   if (100 - x > 100 - y){
+       return `${x} is farther away from 100 compared to y`;
+   }else if (100 - x === 100-y){
+       return `${x} and ${y} are equal`
+   }else{
+       return `${y} is closer to 100 than ${x}`
+   }
+}
+console.log(closeTo100(10, 15));
+console.log(closeTo100(50, 50));
+
+function occurrences(str, ch){
+    let x = str.split("");
+    let y = x.filter(value => value === ch);
+    y.length;
+    if (y.length >= 2 && y.length < 5) {
+        return true;
+    }else{
+        return false;
+    }
+}
+console.log(occurrences("actually", "a"));
+console.log(occurrences("actually", "y"));
+console.log(occurrences("oooooh", "o"));
+
+function myArrays(arr){
+    let y = arr.filter(myFunction);
+    function myFunction(value){
+        return value % 2 === 0;
+    }
+    return y.length;
+}
+console.log(myArrays([2,4,6,8,9]));
+console.log(myArrays([1,3,5,7,9]));
+
+function anArray(num){
+    const generatedArray = [];
+    for (let i = 1; i <= num; i += 1){
+        generatedArray.push(i);
+    }
+    let y = generatedArray.filter(myFunction);
+    function myFunction(value) {
+    return value % 2 === 0;
+    }
+    return y.length;
+}
+console.log(anArray(6));
+
+function sortingArrays(arr){
+    for (let i = 0; i < arr.length; i++){
+        if (arr[i + 1] < arr[i]){
+            return "Falseeeee!";
+        }else{
+            return "Ascending!";
+        }
+    }
+}
+console.log(sortingArrays([5,4,3,6]));  
+
+function largestEvenNumber(arr) {
+let y = arr.filter(myFunction);
+function myFunction(value) {
+    return value % 2 === 0;
+}
+return Math.max.apply(null,y);
+}
+console.log(largestEvenNumber([40,20,50,90,120,200]));
+console.log(largestEvenNumber([440,20,50,90,120,200]));
+
+function leapYear(year){
+    if (year % 4 === 0) {
+        return "leap year";
+    }else{
+        return "Not a  leap year";
+    }
+}
+console.log(leapYear(2020));
+
+const objA ={
+    a : 1,
+    b : 2,
+    c : 1
+};
+const objB ={
+    a : 1,
+    b : 1,
+    c : 1
+};
+const objC ={
+    a : 1,
+    b : 1,
+    d : 1
+};
+function objectEqual(a, b) {
+    Object.keys(a).every(myFunction);
+    function myFunction(key){
+        return key === Object.keys(b);
+    }
+}
+console.log(objectEqual(objA, objC));
